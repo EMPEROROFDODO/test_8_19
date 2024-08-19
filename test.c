6112a7575e9d12f5f24a56f302667f6e40e58e2e
ASUS <ASUS@LAPTOP-1V72JD3E>
2024-08-19 22:53:45 +0800

    添加项目文件。

---
+++ test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//int main()
+//{
+//	//printf("c:\test\test.c");
+//	//printf("ab\ncd");
+//
+//
+//	return 
+int main()
+{
+	////printf("%c\n",'\'');
+	//printf("%s\n","abc");
+	//printf("%s\n", "a");
+	//printf("%s\n", "\"");
+	//printf("c:\\test\\test.c");
+	//printf("\a");
+	//printf("?");
+	//printf("%c\n", '\x30');  //ascciiֵ
+	return 0;
+}
